8b9b4f38dec3bcbc4272b28eb5cb8a7f4b823863
Joseph Shambrook <josephshambrook@googlemail.com>
2016-05-02 18:16:37 +0100

    More updated content

--- weddingapp/forms.py
+++ weddingapp/forms.py
@@ -4,9 +4,13 @@ from weddingapp.models import Guest
 
 
 class ExtraForm(forms.ModelForm):
-    diet = forms.CharField(max_length=128, help_text="Please enter any dietary requirements for :",
+    diet = forms.CharField(max_length=128,
+                           help_text="Please enter any dietary requirements for :",
                            required=False)
-    transport = forms.BooleanField(initial=False, help_text="Will you be needing transport?", required=False)
+    transport = forms.BooleanField(
+        initial=False,
+        help_text="Would /guest name/ be interested in transport from the church to the reception?",
+        required=False)
 
     # An inline class to provide additional information on the form.
     class Meta:
