1a62ff7e878b5429d958720f27fb7d6f722e2dc4
Joseph Shambrook <josephshambrook@googlemail.com>
2016-05-09 19:58:01 +0100

    Stuff for Heroku. Updated content

--- weddingapp/forms.py
+++ weddingapp/forms.py
@@ -6,12 +6,15 @@ from weddingapp.models import Guest
 class ExtraForm(forms.ModelForm):
     diet = forms.CharField(max_length=128,
                            help_text=_("Please enter any dietary requirements for :"),
-                           required=False)
+                           required=False,
+                           widget=forms.TextInput(attrs={'class': 'form-control'})
+                           )
 
     transport = forms.BooleanField(
         initial=False,
         help_text=_("Would /guest name/ be interested in transport from the church to the reception?"),
-        required=False)
+        required=False
+    )
 
     # An inline class to provide additional information on the form.
     class Meta:
