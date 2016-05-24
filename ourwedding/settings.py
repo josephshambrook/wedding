d1f2dbca91f7f69a4b9bc54ca2897e18f0f2d663
Joseph Shambrook <josephshambrook@googlemail.com>
2016-05-24 22:13:30 +0100

    trying this

--- ourwedding/settings.py
+++ ourwedding/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['joe-and-lou.co.uk']
+ALLOWED_HOSTS = ['www.joe-and-lou.co.uk', 'joe-and-lou.co.uk', 'whispering-fjord-33511.herokuapp.com']
 
 # Application definition
 
