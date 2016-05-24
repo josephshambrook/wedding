40aa3fe8b0348f626a63d735ff54e33fa1184200
Joseph Shambrook <josephshambrook@googlemail.com>
2016-05-24 21:59:08 +0100

    trying this

--- ourwedding/settings.py
+++ ourwedding/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.joe-and-lou.co.uk']
+ALLOWED_HOSTS = ['joe-and-lou.co.uk']
 
 # Application definition
 
