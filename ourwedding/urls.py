e3f4abe740458ef5ea8ea7af06aee43222792c2c
Joseph Shambrook <josephshambrook@googlemail.com>
2016-05-28 15:08:05 +0100

    homepage now working

--- ourwedding/urls.py
+++ ourwedding/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from weddingapp.views import home_view
+
 urlpatterns = [
-    # url(r'^$', include('weddingapp.urls')),
+    url(r'^$', home_view, name='home'),
     url(r'^rsvp/', include('weddingapp.rsvp_urls')),
     url(r'^faq/', include('weddingapp.faq_urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^i18n/', include('django.conf.urls.i18n')),
 ]
