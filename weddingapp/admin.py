95d300bb1ca0a7ee6e4ebb02a63d2f3e30ff0068
Joseph Shambrook <josephshambrook@googlemail.com>
2016-07-12 15:14:38 +0100

    improvements to the admin interface, so we can see our collected data more easily

--- weddingapp/admin.py
+++ weddingapp/admin.py
@@ -2,5 +2,28 @@ from django.contrib import admin
 
 from .models import Invite, Guest
 
-admin.site.register(Invite)
-admin.site.register(Guest)
+
+class GuestInline(admin.TabularInline):
+    model = Guest
+    extra = 0
+    min_num = 2
+
+
+class InviteAdmin(admin.ModelAdmin):
+    # define which columns show
+    list_display = ('group_name', 'code', 'group_count',)
+    # define which filters to show
+    list_filter = ('code',)
+    # show guest data inline
+    inlines = [GuestInline]
+
+
+class GuestAdmin(admin.ModelAdmin):
+    # define which columns show
+    list_display = ('guest_name', 'attending', 'diet', 'transport',)
+    # define which filters to show
+    list_filter = ('guest_name', 'invite', 'attending',)
+
+
+admin.site.register(Invite, InviteAdmin)
+admin.site.register(Guest, GuestAdmin)
