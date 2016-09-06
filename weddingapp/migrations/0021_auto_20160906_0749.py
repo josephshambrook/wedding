6e6f404f01be8e2fc7cab4f060465544664769d6
Joseph Shambrook <josephshambrook@googlemail.com>
2016-09-06 10:02:30 +0100

    fixing still

--- weddingapp/migrations/0021_auto_20160906_0749.py
+++ weddingapp/migrations/0021_auto_20160906_0749.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('weddingapp', '0020_auto_20160906_0743'),
+        ('weddingapp', '0019_auto_20160905_2237'),
     ]
 
     operations = [
